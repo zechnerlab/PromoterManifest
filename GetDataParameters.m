700a60d20fe92c04a13a69925e95865dba311ea8
zechner <zechner@mpi-cbg.de>
2020-10-06 10:38:35 +0200

    Deleting old files

--- GetDataParameters.m
+++
@@ -1,9 +0,0 @@
-function [promoters, concentrations, numPulses, durations, intervals] = GetDataParameters(dataB)
-
-    promoters = unique(dataB.Promoter)';
-    concentrations = unique(dataB.Condition(:, 1));
-    numPulses = unique(dataB.Condition(:, 2));
-    durations = unique(dataB.Condition(:, 3));
-    intervals = unique(dataB.Condition(:, 4));
-    intervals = setdiff(intervals, -1);
-end
